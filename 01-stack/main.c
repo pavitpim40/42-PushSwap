80c639510d404b07764f0a46555ffd5bf3d39bf7
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-07 04:05:15 +0700

    swap-head

--- 01-stack/main.c
+++ 01-stack/main.c
@@ -25,7 +25,7 @@ Stack *create_stack()
 	return stack;
 }
 
-void	push(Stack *stack, int data)
+void push(Stack *stack, int data)
 {
 	Node* node = (Node *)malloc(sizeof(Node));
 	if(node == NULL)
@@ -33,7 +33,7 @@ void	push(Stack *stack, int data)
 	node->data = data;
 	node->next = NULL;
 	node->prev = stack->top; // if 1st element to push ?
-	printf("STACK->TOP %p\n",stack->top);
+	// printf("STACK->TOP %p\n",stack->top);
 	// stack is not empty before
 	if(stack->top != NULL)
 		stack->top->next = node;
@@ -58,6 +58,31 @@ int	pop(Stack* stack)
 	return data;
 }
 
+void swap_head(Stack *stack)
+{
+	if(stack->size < 2)
+		return;
+	
+	Node* first = stack->top;
+	Node* second = first->prev;
+	Node* third = second->prev;
+
+	first->prev = third;
+	if(third != NULL)
+		third->next = first;
+	first->next = second;
+	second->prev = first;
+	second->next = NULL;
+	
+
+	if(third == NULL)
+		stack->bottom = first;
+	// or
+	// if (stack->bottom == first) {
+    //     stack->bottom = second;
+    // }
+	stack->top = second;
+}
 void print_stack(Stack* stack) {
     printf("Stack: ");
     Node* node = stack->top;
@@ -79,18 +104,11 @@ int main() {
 
     print_stack(stack);
 
-	pop(stack);
+	// pop(stack);
+	swap_head(stack);
 	print_stack(stack);
 
-		pop(stack);
-	print_stack(stack);
-		pop(stack);
-	print_stack(stack);
-		pop(stack);
-	print_stack(stack);
-		pop(stack);
-	print_stack(stack);
-		pop(stack);
-	print_stack(stack);
+	printf("BOTT %d\n", stack->bottom->data);
+	
 
 }
