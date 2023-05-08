533508927fb8d395eba863eac55d268697c9704a
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-09 01:14:31 +0700

    small sort, i think it bug in some case

--- 07-push-swap-v3.0-sort-mand/srcs/sort/five_sort.c
+++ 07-push-swap-v3.0-sort-mand/srcs/sort/five_sort.c
@@ -6,11 +6,13 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 00:53:32 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/09 00:55:14 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/09 01:14:07 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+
 
 void five_sort(Stack *stack_a,Stack *stack_b)
 {
@@ -20,4 +22,17 @@ void five_sort(Stack *stack_a,Stack *stack_b)
 	swap_sort(stack_b);
 	p_move_top(stack_b,stack_a);
 	p_move_top(stack_b,stack_a);
+}
+
+
+
+
+void small_sort(Stack *stack_a,Stack *stack_b)
+{
+	if(stack_a->size == 2)
+		swap_sort(stack_a);
+	else if(stack_a->size == 3)
+		triple_sort(stack_a);
+	else if(stack_a->size == 5)
+		five_sort(stack_a,stack_b);
 }
