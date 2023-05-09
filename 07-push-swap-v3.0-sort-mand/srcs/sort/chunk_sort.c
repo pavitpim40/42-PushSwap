be9c6af67ae2b3bd5be55b5619ac12fddcf7b70c
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-10 04:21:30 +0700

    success push from two side

--- 07-push-swap-v3.0-sort-mand/srcs/sort/chunk_sort.c
+++ 07-push-swap-v3.0-sort-mand/srcs/sort/chunk_sort.c
@@ -6,7 +6,7 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:14:31 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/10 03:29:14 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/10 04:20:47 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,6 @@
 // 	print_stack(B);
 // }
 
-
-
 // # OK : BUT ONLY FROM TOP
 
 // void chunk_sort(Stack *A, Stack *B)
@@ -64,14 +62,13 @@
 // 		chunk_size = 5;
 // 	else
 // 		chunk_size = 10;
-	
+
 // 	Node *curr_top = A->top;
 // 	// Node *from_bot = A->bottom;
 // 	int span = A->size / chunk_size;
 // 	int round = 1;
 // 	int max_in_round = round * span;
 // 	int min_in_round = (round - 1)*span + 1;
-
 
 // 	printf("min in round %d\n",min_in_round);
 // 	printf("max in round %d\n",max_in_round);
@@ -91,8 +88,8 @@
 // 			p_move_top(A,B);
 // 			count=0;
 // 			// print_stack(B);
-// 		} 
-// 		else 
+// 		}
+// 		else
 // 		{
 
 // 			count++;
@@ -112,14 +109,13 @@
 // 		chunk_size = 5;
 // 	else
 // 		chunk_size = 10;
-	
+
 // 	Node *curr_top = A->top;
 // 	// Node *from_bot = A->bottom;
 // 	int span = A->size / chunk_size;
 // 	int round = 1;
 // 	int max_in_round = round * span;
 // 	int min_in_round = (round - 1)*span + 1;
-
 
 // 	printf("min in round %d\n",min_in_round);
 // 	printf("max in round %d\n",max_in_round);
@@ -139,8 +135,8 @@
 // 			p_move_top(A,B);
 // 			count=0;
 // 			// print_stack(B);
-// 		} 
-// 		else 
+// 		}
+// 		else
 // 		{
 
 // 			count++;
@@ -153,30 +149,67 @@
 
 void chunk_sort(Stack *A, Stack *B)
 {
-	Node *f_top = A->top;	
+	Node *f_top = A->top;
+	Node *f_bot = A->bottom;
 	int c_top = 0;
+	int c_bot = 0;
 	// Node *f_bot = A->bottom;
-
+	int fake_count = 20;
 	// #1 ทำจน A หมด
-	while(A->size)
-	{
-		// #2 ทำทีละ chunk; 
-		int chunk_size = 20;
-		while(chunk_size)
-		{
-
-			//#3.1 ดูว่าไล่กี่ตัววถึงจะเจอใน range
-			while(f_top->data < 1 || f_top->data > 20)
-				c_top++;
-			//#3.2 หมุนลงล่างตาม c_top
-			while(c_top--)
-				r_shift_up(A,NULL);
-			printf("PUSH EL = %d\n", f_top->data);
-			p_move_top(A,B);
-			c_top=0;
-			f_top = A->top;
-			chunk_size--;
-		}
+	int mid = 5; // control ว่าหากี่ตัว
+	while (fake_count)
+	{	
+		// printf("a\n");
+		// #2 ทำทีละ chunk;
 		
+		int current_size = A->size;
+		while (mid)
+		{
+			// printf("NEW ROUND , mid = %d\n",mid);
+			// #3.1 ดูว่าไล่กี่ตัววถึงจะเจอใน range
+			while ((f_top->data < 1 || f_top->data > 20) && c_top <= A->size/2)
+			{
+				c_top++;
+				f_top = f_top->prev;
+			}
+			while((f_bot->data < 1 || f_bot->data > 20) && c_bot<= A->size/2)
+			{	
+				c_bot++;
+				f_bot = f_bot->next;
+			}
+			printf("-------------ROUND : %d -------------\n", 6-mid);
+			print_stack(A);
+			printf("c_top=%d\n",c_top);
+			printf("c_bot=%d\n",c_bot);
+			printf("curren_size=%d\n",current_size);
+			if (c_top - 1 <=  c_bot )
+			{
+				while (c_top--)
+					r_shift_up(A, NULL);
+				printf("PUSH EL from TOP = %d\n", f_top->data);
+				p_move_top(A, B);
+				
+			}
+			else
+			{
+				while (c_bot--)
+					rr_shift_down(A, NULL);
+				rr_shift_down(A, NULL);
+				printf("PUSH EL from BOT = %d\n", f_bot->data);
+				p_move_top(A, B);
+				
+			}
+		
+			f_top = A->top;
+			
+			f_bot = A->bottom;
+			// #3.2 หมุนลงล่างตาม c_top
+			c_top = 0;
+			c_bot = 0;
+			mid--;
+			printf("BEF NEW ROUND , mid = %d\n",mid);
+		}
+		fake_count--;
 	}
+	
 }
