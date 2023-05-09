285a3665091069950421986835b5498b27e115a2
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-10 01:16:03 +0700

    re-architect pre_sort

--- 07-push-swap-v3.0-sort-mand/srcs/sort/chunk_sort.c
+++ 07-push-swap-v3.0-sort-mand/srcs/sort/chunk_sort.c
@@ -6,7 +6,7 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:14:31 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/10 01:12:19 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/10 01:14:44 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,44 +51,3 @@ void chunk_sort(Stack *A, Stack *B)
 	print_stack(B);
 }
 
-static int find_min(Stack *A)
-{
-	Node *current;
-	int min;
-
-	current = A->top;
-	min = INT_MAX;
-	while (current)
-	{
-		if (current->rank == -1 && current->data < min)
-			min = current->data;
-		current = current->prev;
-	}
-	return (min);
-}
-void label_rank(Stack *A)
-{
-	int rank;
-	int min;
-	Node *current;
-
-	rank = 1;
-	min = INT_MAX;
-	current = A->top;
-	while (rank <= A->size)
-	{
-		min = find_min(A);
-		current = A->top;
-		while (current)
-		{
-			if (current->data == min)
-			{
-				current->rank = rank;
-				rank++;
-				min = INT_MAX;
-				break;
-			}
-			current = current->prev;
-		}
-	}
-}
