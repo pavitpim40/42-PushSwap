4ad046e7abe15f25855e6a267d8a07bbc5c99b78
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-11 19:03:30 +0700

    refactor util and chunk-sort

--- 08-push-swap-v3.1-chunk-with-refactor/srcs/sort/chunk_sort.c
+++ 08-push-swap-v3.1-chunk-with-refactor/srcs/sort/chunk_sort.c
@@ -6,56 +6,58 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:14:31 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/11 18:51:35 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/11 19:01:07 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-void move_chunk_in_range(Stack *A, Stack *B, int loop_count, int chunk_size, int swap_down)
+static void move_chunk_in_range(Stack *A, Stack *B,  int chunk_size,int loop_count, int swap_down)
 {
 	int start = (loop_count - 1) * chunk_size + 1;
 	int end = (loop_count)*chunk_size;
-	Node *f_top;
-	Node *f_bot;
-	int c_top;
-	int c_bot;
-	// move_num_in_range(A,B,start,end);
+
 	while (chunk_size--)
 	{
-		find_num_in_range_and_move(A,B,start,end);
+		find_num_in_range_and_move(A, B, start, end);
 		if (swap_down == 1)
 			r_shift_up(B, NULL);
 	}
 }
 
+static int calc_chunk_count(Stack *A)
+{
+	if (A->size <= 100)
+		return (5) ;
+	else if (A->size <= 500)
+		return (11);
+	else
+		return 5;
+}
 
 void chunk_sort(Stack *A, Stack *B)
 {
-	int chunk_count;
-	chunk_count = 0;
-	if (A->size <= 100)
-		chunk_count = 5;
-	else if (A->size <= 500)
-		chunk_count = 11;
+	int chunk_size;
+	int loop_count;
+	int is_swap_down;
 
-	int chunk_size = A->size / chunk_count;
-	int loop_count = 1;
-	int is_swap_down = 0;
-
-	while (chunk_count)
+	chunk_size = A->size / calc_chunk_count(A);;
+	loop_count = 1;
+	is_swap_down = 0;
+	while (A->size)
 	{
-		move_chunk_in_range(A, B, loop_count, chunk_size, 0);
+		move_chunk_in_range(A, B, chunk_size, loop_count, 0);
 		if (is_swap_down == 0)
 			is_swap_down = 1;
 		else
 			is_swap_down = 0;
-
 		loop_count++;
-		chunk_count--;
 		if (A->size == 4)
+		{
 			forth_sort(A, B);
+			break;
+		}
 	}
 	while (B->size)
-		find_num_and_move(B,A,B->size);
+		find_num_and_move(B, A, B->size);
 }
