0f6244743092058495233032724c2a295f4e58ec
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-11 17:17:17 +0700

    finish refractor input

--- 08-push-swap-v4.0-turk/srcs/input/input_fill.c
+++ 08-push-swap-v4.0-turk/srcs/input/input_fill.c
@@ -6,64 +6,56 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 17:45:31 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/11 16:58:52 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/11 17:16:35 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+static int try_to_fill_arg(Stack *A, char *each_arg)
+{
+	char **words;
+	int j;
+
+	words = ft_split(each_arg, ' ');
+	j = 0;
+	while (words[j])
+		j++;
+	j--;
+	while (j >= 0)
+	{
+		if (!verify_string(A, words[j]))
+		{
+			free(words[j]);
+			free(words);
+			free_stack(A);
+			return 0;
+		}
+		else
+			push_with_index(A, ft_atoi(words[j]), -1);
+		free(words[j]);
+		j--;
+	}
+	free(words);
+	return (1);
+}
 
 
-
-// return  0 : not valid
-// return 1 : valid
 Stack *fill_stack(int argc, char **argv)
 {
+	int success_fill;
+	Stack *A;
 
-	int i;
-	int j;
-	// int len;
-	char **words;
-	Stack *stack;
-
-
-	i = argc - 1; 
-	j = 0;
-	// len = 0;
-	stack = create_stack("a");
-	while (i > 0)
+	A = create_stack("a");
+	success_fill = 0;
+	while (argc > 1)
 	{
-		if (ft_strlen(*(argv + i)) == 0)
+		if (ft_strlen(argv[argc - 1]) == 0)
 			return 0;
-		// เป็น string แต่อาจจะเป็น stringที่ spilt ได้/ไม่ได้ ก็ได้
-		
-		words = ft_split(*(argv + i), ' ');
-		j = 0;
-		while(words[j])
-			j++;
-		j--;
-		while (j >= 0)
-		{
-
-			if (!verify_string(stack,words[j]))
-			{
-
-			
-				free(words[j]);
-				free(words);
-				free_stack(stack);
-				return NULL;
-			
-			}
-			else
-		
-				push_with_index(stack, ft_atoi(words[j]),-1);
-			free(words[j]);
-			j--;
-		}
-		free(words);
-		i--;
+		success_fill = try_to_fill_arg(A, argv[argc - 1]);
+		if (!success_fill)
+			return NULL;
+		argc--;
 	}
-
-	return stack;
+	return A;
 }
--- 08-push-swap-v4.0-turk/srcs/input/input_validate.c
+++ 08-push-swap-v4.0-turk/srcs/input/input_validate.c
@@ -6,7 +6,7 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 16:12:45 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/11 16:59:09 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/11 17:11:35 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,10 @@ static int is_nan(char *word)
 
 	i = 0;
 	found_num = 0;
-
 	if (*(word + i) == '\0')
 		return (0);
 	while (i < (int)ft_strlen(word) && *(word + i))
 	{
-		// printf("i = %d\n",i);
 		if (is_sign(*(word + i)) && found_num == 0)
 			i++;
 		else if (ft_isdigit(*(word + i)))
@@ -40,39 +38,25 @@ static int is_nan(char *word)
 			found_num = 1;
 		}
 		else
-		{
-			// printf("exit\n");
 			return (1);
-		}
 	}
-	return (0); // truthy
+	return (0);
 }
 
-
-static int is_overflow(char *string)
+static int is_overflow(char *word)
 {
-	int num = ft_atoi(string);
-	char *string_check = ft_itoa(num);
+	int num = ft_atoi(word);
+	char *word_check = ft_itoa(num);
 	int i = 0;
 	int j = 0;
 
-	
-	while (string[i] && string_check[j])
+	while (word[i] && word_check[j])
 	{
-		// printf("in loop with str-check %s\n", string_check);
-		if (string[i] == string_check[j])
-		{
-			i++;
-			j++;
+		if (word[i++] == word_check[j++])
 			continue;
-		}
 		else
-		{
-			// printf("Over flow\n");
 			return (1);
-		}
 	}
-	// printf("Not Over flow\n");
 	return (0);
 }
 
@@ -89,9 +73,9 @@ static int is_dup(Stack *stack, int num)
 	return (0);
 }
 
-int verify_string(Stack* A,char *word)
+int verify_string(Stack *A, char *word)
 {
-	if(is_nan(word) || is_overflow(word) || is_dup(A,ft_atoi(word)))
+	if (is_nan(word) || is_overflow(word) || is_dup(A, ft_atoi(word)))
 		return 0;
 	return 1;
 }
