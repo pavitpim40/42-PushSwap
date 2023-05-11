0601c1e08a90be24cb07d2cac9bfcbc2d4b95514
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-11 16:47:36 +0700

    setup v4.0 agian

---
+++ 08-push-swap-v4.0-turk/srcs/main.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 13:12:34 by ppimchan          #+#    #+#             */
+/*   Updated: 2023/05/11 16:22:54 by ppimchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+// FOR INPUT
+int main(int argc, char *argv[])
+{
+
+	// printf("hi\n");
+	Stack *stack_A;
+	Stack *stack_B;
+
+	// ### CHECK VALID ARGS
+	stack_A = input_check(argc, argv);
+	stack_B = create_stack("b");
+	if (!stack_A)
+	{
+		// 	free_stack(stack_A);
+		free_stack(stack_B);
+		write(2, "Error\n", 6);
+		return (1);
+	}
+	// PRESORTED
+	label_rank(stack_A);
+	if (stack_A->size == 2)
+		swap_sort(stack_A);
+	else if (stack_A->size == 3)
+		triple_sort(stack_A);
+	else if (stack_A->size == 4)
+		forth_sort(stack_A,stack_B);
+	else if (stack_A->size == 5)
+		five_sort(stack_A,stack_B);
+	else
+		chunk_sort(stack_A, stack_B);
+
+	// write(1,"OK\n",3);
+	// print_stack(stack_A);
+	// printf("======================================\n");
+	// printf("==========  SORTED STACK-A  ==========\n");
+	// printf("======================================\n");
+	// print_stack(stack_A);
+	// ### EXIT
+	free_stack(stack_A);
+	free_stack(stack_B);
+}
---
+++ 08-push-swap-v4.0-turk/srcs/sort/bubble_sort.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bubble_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/07 21:06:40 by ppimchan          #+#    #+#             */
+/*   Updated: 2023/05/10 20:01:36 by ppimchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+static void do_bubble(Stack *stack_a, Stack *stack_b)
+{
+	if (stack_a->top->prev == stack_a->bottom)
+		s_swap_top(stack_a);
+	else
+	{
+		s_swap_top(stack_a);
+		p_move_top(stack_a, stack_b);
+	}
+}
+
+void bubble_sort(Stack *stack_a, Stack *stack_b)
+{
+	Node *element_1;
+	Node *element_2;
+	int count_inner;
+	int count;
+	int size;
+
+	size = stack_a->size;
+	count = 0;
+
+	while (count++ < size - 1)
+	{
+
+		element_1 = stack_a->top;
+		element_2 = element_1->prev;
+		count_inner = 0;
+		while (count_inner++ < size - count)
+		{
+			if (element_1->data > element_2->data)
+				do_bubble(stack_a, stack_b);
+			else
+				p_move_top(stack_a, stack_b);
+			element_1 = stack_a->top;
+			element_2 = element_1->prev;
+		}
+		while (stack_a->size < size)
+			p_move_top(stack_b, stack_a);
+	}
+}
