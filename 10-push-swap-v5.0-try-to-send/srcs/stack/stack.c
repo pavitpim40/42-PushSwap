1ea1ee96197b1c5029e5154356a4fc3f0baad807
Pavit Pimchanagul <ppimchan@z2t19c4.42bangkok.com>
2023-05-16 17:19:57 +0700

    refactor stack_util

--- 10-push-swap-v5.0-try-to-send/srcs/stack/stack.c
+++ 10-push-swap-v5.0-try-to-send/srcs/stack/stack.c
@@ -6,15 +6,15 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:32:07 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/15 17:58:08 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:17:19 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-t_stack *create_stack(char *name)
+t_stack	*create_stack(char *name)
 {
-	t_stack *new_stack;
+	t_stack	*new_stack;
 
 	new_stack = (t_stack *)malloc(sizeof(t_stack));
 	if (new_stack == NULL)
@@ -28,13 +28,13 @@ t_stack *create_stack(char *name)
 	return (new_stack);
 }
 
-void free_stack(t_stack *stack)
+void	free_stack(t_stack *stack)
 {
-	t_node *current;
-	t_node *next;
+	t_node	*current;
+	t_node	*next;
 
 	if (!stack)
-		return;
+		return ;
 	current = stack->top;
 	while (current != NULL)
 	{
@@ -46,13 +46,13 @@ void free_stack(t_stack *stack)
 	free(stack);
 }
 
-void push(t_stack *stack, int data)
+void	push(t_stack *stack, int data)
 {
-	t_node *node;
+	t_node	*node;
 
 	node = (t_node *)malloc(sizeof(t_node));
 	if (node == NULL)
-		return;
+		return ;
 	node->data = data;
 	node->next = NULL;
 	node->prev = stack->top;
@@ -64,13 +64,13 @@ void push(t_stack *stack, int data)
 	stack->size++;
 }
 
-void push_with_rank(t_stack *dst, int data, int rank)
+void	push_with_rank(t_stack *dst, int data, int rank)
 {
-	t_node *node;
+	t_node	*node;
 
 	node = (t_node *)malloc(sizeof(t_node));
 	if (node == NULL)
-		return;
+		return ;
 	node->data = data;
 	node->next = NULL;
 	node->prev = dst->top;
@@ -79,21 +79,11 @@ void push_with_rank(t_stack *dst, int data, int rank)
 		dst->max = data;
 	if (rank > dst->max_rank)
 		dst->max_rank = rank;
-	// printf("\n######## PUSH WITH RANK ############\n");
-	// printf("STACK : %s\n", dst->name);
-	// printf("___data = %d\n", data);
-	// printf("___rank = %d\n", rank);
-	// printf("dst->min = %d\n", dst->min);
-	// printf("dst->max = %d\n", dst->max);
-	// printf("dst->min_rank = %d\n", dst->min_rank);
-	// printf("dst->max_rank = %d\n", dst->max_rank);
 	if (data < dst->min)
 	{
 		dst->min = data;
 		dst->min_rank = rank;
 	}
-	if (rank != -1 && rank < dst->min_rank)
-		dst->min_rank = rank; // why cannot move ?
 	if (dst->top != NULL)
 		dst->top->next = node;
 	dst->top = node;
--- 10-push-swap-v5.0-try-to-send/srcs/stack/stack_utils.c
+++ 10-push-swap-v5.0-try-to-send/srcs/stack/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 18:36:01 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/05/15 18:51:59 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:19:29 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	find_next_min(t_stack *src)
 	}
 	return (next_min);
 }
+
 int	find_next_max(t_stack *src)
 {
 	t_node	*current;
@@ -74,6 +75,7 @@ int	find_next_max_rank(t_stack *src)
 	}
 	return (next_max_rank);
 }
+
 int	pop(t_stack *stack)
 {
 	int		data;
@@ -87,37 +89,34 @@ int	pop(t_stack *stack)
 	if (stack->top != NULL)
 	{
 		stack->top->next = NULL;
-		// printf("_______________POP_______________\n");
-		// printf("temp->rank = %d\n",temp->rank);
-		// printf("stack->min = %d\n",temp->rank);
 		if (temp->rank == stack->min_rank)
 		{
 			stack->min_rank = find_next_min_rank(stack);
 			stack->min = find_next_min(stack);
-			
 		}
 		if (temp->rank == stack->max_rank)
 		{
 			stack->max_rank = find_next_max_rank(stack);
 			stack->max = find_next_max(stack);
 		}
-			
 	}
 	stack->size--;
 	free(temp);
 	return (data);
 }
 
-void print_stack(t_stack *stack)
-{
-	printf("Stack: %s\n", stack->name);
-	if (!stack)
-		return;
-	t_node *node = stack->top;
-	while (node != NULL)
-	{
-		printf("data = %d rank = %d \n", node->data, node->rank);
-		node = node->prev;
-	}
-	printf("\n");
-}
+// void	print_stack(t_stack *stack)
+// {
+// 	t_node *node;
+
+// 	printf("Stack: %s\n", stack->name);
+// 	if (!stack)
+// 		return ;
+// 	node = stack->top;
+// 	while (node != NULL)
+// 	{
+// 		printf("data = %d rank = %d \n", node->data, node->rank);
+// 		node = node->prev;
+// 	}
+// 	printf("\n");
+// }
